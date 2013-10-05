7d16ff30ef2953538a3e17a7ff9f1f0efc550b61
Jim V <vlandham@gmail.com>
2013-10-04 20:50:08 -0700

    ignore node_modules

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'ZcCiuN3i7hCGiOi4JnLEjw',
+  consumer_secret:      'r2s7MCL3f713BMkGP7hCMQ4o6MuH2hOCm59ax1PN8',
+  access_token:         '1936234152-6Vo0UC7Cvae0mh07VRfeUt13TiTOnOdFaqL4ZvO',
+  access_token_secret:  '5anwoeUlxj3xgxMWxRSk4pw2GuaY31VkBbOBWFKdKkc'
+}
+
