33e047a7c6cd3dbfd2c27947aea5380006932966
Jim V <vlandham@gmail.com>
2013-10-04 21:37:12 -0700

    in bed

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 // Our Twitter library
 var Twit = require('twit');
+var $ = require('cheerio');
+var request = require('request');
+var express = require("express");
+var _ = require("underscore");
 
 if (process.env.REDISTOGO_URL) {
 // TODO: redistogo connection
@@ -15,7 +19,6 @@ redis.on("error", function (err) {
   console.log("Error " + err);
 });
 
-var express = require("express");
 var app = express();
 app.use(express.logger());
 
@@ -34,6 +37,8 @@ var T = new Twit(require('./config.js'));
 // This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
 var mediaArtsSearch = {q: "#yolo", count: 10, result_type: "recent"}; 
 
+var domain = 'https://news.google.com/'
+
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
 	T.get('search/tweets', mediaArtsSearch, function (error, data) {
@@ -42,16 +47,8 @@ function retweetLatest() {
 	  if (!error) {
 	  	// ...then we grab the ID of the tweet we want to retweet...
 		var retweetId = data.statuses[0].id_str;
+    retweet(retweetId);
 		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
 	  }
 	  // However, if our original search request had an error, we want to print it out here.
 	  else {
@@ -60,8 +57,54 @@ function retweetLatest() {
 	});
 }
 
+function tweet(text) {
+  T.post('statuses/update', {status: text }, function (error, response) {
+    if (response) {
+      console.log('Success! Check your bot, it should have retweeted something.')
+    }
+    // If there was an error with our Twitter call, we print it out here.
+    if (error) {
+      console.log('There was an error with Twitter:', error);
+    }
+  })
+}
+
+function retweet(retweetId) {
+  T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+    if (response) {
+      console.log('Success! Check your bot, it should have retweeted something.')
+    }
+    // If there was an error with our Twitter call, we print it out here.
+    if (error) {
+      console.log('There was an error with Twitter:', error);
+    }
+  })
+}
+
+function gotHTML(err, resp, html) {
+  if (err) return console.error(err);
+  var parsedHTML = $.load(html);
+  // get all img tags and loop over them
+  var headers = [];
+  _.values(parsedHTML('.titletext')).forEach(function(link) {
+    var text = $(link).text();
+    console.log(text);
+    headers.push(text)
+  });
+  var randomIndex = Math.floor(Math.random() * headers.length);
+  console.log("random:")
+  console.log(headers[randomIndex]);
+  tweet(headers[randomIndex] + " ...in bed");
+
+}
+
+function getTweet() {
+  request(domain, gotHTML)
+}
+
 // Try to retweet something as soon as we run the program...
-retweetLatest();
+getTweet();
+//retweetLatest();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetLatest, 1000 * 60 * 60);
